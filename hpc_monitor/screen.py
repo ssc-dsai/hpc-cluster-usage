e7df2f580ec2a2bac20f103cbe942cadaf553d12
Peter Boyd <peter.g.boyd@gmail.com>
2025-03-20 15:34:51 +0000

    sorted out display of nodes with many CPUs

--- hpc_monitor/screen.py
+++ hpc_monitor/screen.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+from math import ceil
 import sys
 import getpass
 import pyfiglet
@@ -17,10 +18,19 @@ class Display:
             self._width, self._height = 100, 70
 
         # index errors :(
-        self._width, self._height = (2000, 200)
+        self._width, self._height = (2000, 900)
+        # maximum length a node can be represented on a terminal line
+        self.max_repr_len = 64
         self._screen = np.empty((self._height, self._width), dtype='U36')
+        self._screen[:] = " "
         self._usercodes = OrderedDict()
         self._cluster_stat = None
+
+    @property
+    def flush_screen(self):
+        del self._screen
+        self._screen = np.empty((self._height, self._width), dtype='U36')
+        self._screen[:] = " "
 
     @property
     def cluster_stat(self):
@@ -47,17 +57,28 @@ class Display:
 
     def format_output(self, cluster):
         """Prepare the _screen array with colors and such."""
+        self.flush_screen
         if self.cluster_stat is None:
             print(f"Warning: format_output was called but cluster_stat was not defined!")
             return
-
+        
         # formatting is cluster-specific
         lcolumn = 0
-        row = 0
+        row = 0 
         max_line_length = 0
         n_used, n_available, n_total = 0, 0, 0
-        cluster_name = "\033[5m" + pyfiglet.figlet_format(cluster, justify='center', font='standard') + "\033[0m"
-        print(cluster_name)
+        title = pyfiglet.figlet_format(cluster, justify='center', font='standard').split("\n")
+        title[0] = "\033[5m" + title[0]
+        title[-1] = title[-1] + "\033[0m"
+        title = [list(i) for i in title]
+        max_title_len = max(len(t) for t in title)
+        # prepare the multi-line title :)
+        for t in title:
+            self._screen[row, lcolumn:lcolumn+len(t)] = t 
+            row+=1
+
+        default_row = row
+        # now do the nodes
         for node in sorted(self.cluster_stat.resource_list[cluster]):
             desc = self.cluster_stat.resource_desc[cluster][node]
             res = self.cluster_stat.resource_list[cluster][node]
@@ -74,16 +95,53 @@ class Display:
                 s_or_p = "=" if e[0] == "P" else "+"
                 desc[idx] = f"{self._usercodes[user]}{s_or_p}\033[0m"
 
-            line = list(f"\033[1m{node:>13s}\033[0m:") +  ["["] + desc.tolist() + ["]\033[0m"]
-            line = np.array(line, dtype='U36')
-            if len(line) > self._screen.shape[1]:
-                line = line[:self._screen.shape[1]]
-            self._screen[row, lcolumn: lcolumn + len(line)] = line
-            max_line_length = max(len(line), max_line_length)
-            row += 1
+            if node == "ib14gpu-001":
+                pass
+            # if the number of cores is too long for the screen, split the line
+            if desc.shape[0] > self.max_repr_len:
+                # indent for the padding of the sencond+ row(s)
+                indent = 6
+                desc_split = np.array_split(desc, ceil(desc.shape[0]/self.max_repr_len))
+                for idx, sub_desc in enumerate(desc_split):
+                    # first row
+                    if idx == 0:
+                        line = (["\033[1m"] + 
+                                    list(f"{node:>13s}") + 
+                                    ["\033[0m"] + 
+                                    [":", "["] + 
+                                    sub_desc.tolist() + 
+                                    ["\033[0m"] 
+                        )
+
+                        line = np.array(line, dtype='U36')
+                        self._screen[row, lcolumn: lcolumn + len(line)] = line
+                    # middle rows
+                    elif idx < (len(desc_split) - 1):
+                        line = ([" "]*indent) + sub_desc.tolist() + ["\033[0m"]
+                        line = np.array(line, dtype='U36')
+                        self._screen[row, lcolumn: lcolumn + len(line)] = line
+                        # have to account for empty characters (there are 3 in the first row)
+                        self._screen[row, lcolumn+len(line): lcolumn+len(line)+3] = ''
+                    # terminus
+                    else:
+                        line = ([" "]*indent) + sub_desc.tolist() + ["]"]
+                        line = np.array(line, dtype='U36')
+                        self._screen[row, lcolumn: lcolumn + len(line)] = line
+                        # have to account for empty characters (there are 3 in the first row)
+                        self._screen[row, lcolumn+len(line): lcolumn+len(line)+3] = ''
+                    max_line_length = max(len(line), max_line_length)
+                    row += 1
+
+            else:
+                line =  ["\033[1m"] + list(f"{node:>13s}") + ["\033[0m"] + [":", "["] + desc.tolist() + ["\033[0m]"]
+                line = np.array(line, dtype='U36')
+                self._screen[row, lcolumn: lcolumn + len(line)] = line
+                # nb the bash color script codes above do not add to the final output column count.
+                max_line_length = max(len(line), max_line_length)
+                row += 1
         
         lcolumn += max_line_length + 4
-        row = 0
+        row = default_row 
         nline = list(f"{'used:':>12s}{n_used:5d}")
         self._screen[row, lcolumn: lcolumn+len(nline)] = nline
         row += 1
@@ -97,10 +155,10 @@ class Display:
         max_user_length = max(len(x) for x in self.cluster_stat.users[cluster])
 
         h = list([f"{'USER':>{max_user_length}s}", 
-                  f"{'CPU_R':>6s}", 
-                  f"{'CPU_Q':>6s}", 
-                  f"{'GPU_R':>6s}", 
-                  f"{'GPU_Q':>6s}"])
+                  f"{'CPU_R':>8s}", 
+                  f"{'CPU_Q':>8s}", 
+                  f"{'GPU_R':>8s}", 
+                  f"{'GPU_Q':>8s}"])
         nline = np.array(h, dtype='U36')
         self._screen[row, lcolumn:lcolumn+len(nline)] = nline
         row += 1
@@ -121,10 +179,10 @@ class Display:
 
             nline = [f"{self._usercodes[user]}"]
             nline += list([f"{user:>{max_user_length}s}", 
-                           f"{cpu_r:>6d}", 
-                           f"{cpu_q:>6d}",
-                           f"{gpu_r:>6d}",
-                           f"{gpu_q:>6d}",
+                           f"{cpu_r:>8d}", 
+                           f"{cpu_q:>8d}",
+                           f"{gpu_r:>8d}",
+                           f"{gpu_q:>8d}",
                      ])
             nline += ["\033[0m"]
             nline = np.array(nline, dtype='U36')
@@ -141,5 +199,6 @@ class Display:
     def __str__(self):
         all_strings = ""
         for cluster in self.cluster_stat.resource_list:
+            all_strings += "\n"
             all_strings += (self.format_output(cluster))
         return all_strings
